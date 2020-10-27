aca2ec7181f60456d3aaad1be12279fff548d416
unknown <damian9691@wp.pl>
2020-10-27 19:20:50 +0100

    Added logic to forecast option(need to rework styles)

--- src/inputQuery.js
+++ src/inputQuery.js
@@ -15,6 +15,7 @@ export class InputQuery extends Common {
                     fetch(`http://api.weatherapi.com/v1/current.json?key=66667245d6d048b2ad9152824202510&q=${e.path[1][0].value}`)
                     .then(data=> { return data.json()})
                     .then(data=> { 
+                     
                         const arrayFromLink = data['current']['condition']['icon'].split('');
                         const iconNumber = arrayFromLink.slice(arrayFromLink.length-7,arrayFromLink.length-4).join('');
                        
@@ -25,13 +26,13 @@ export class InputQuery extends Common {
                         
                         }  
                         
-                        
-                        this.domElements['weatherIconText'].textContent= data['current']['condition']['text'];
+                        this.domElements['cityName'].textContent = data['location']['name'];
+                        this.domElements['weatherIconText'].textContent = data['current']['condition']['text'];
                         this.domElements['additionalInfoHumidity'].textContent = `${data['current']['humidity']}%`;
                         this.domElements['additionalInfoFeellike'].textContent = `${data['current']['feelslike_c']}°C`;
                         this.domElements['additionalInfoWind'].textContent = `${data['current']['wind_kph']}km/h`;
                         this.domElements['mainInfoTemperature'].textContent = `${data['current']['temp_c']}°C`;
-                        
+                  
                     
                     
                     
--- src/inputQueryForecast.js
+++ src/inputQueryForecast.js
@@ -1,14 +1,23 @@
 import { Common } from '/src/Common.js';
 
 export class ForecastButton extends Common {
-    constructor(input){
+    constructor(button){
         super();
-        this.input= input;
-        this.inputEventListener(input);
+        this.button= button;
+        this.buttonEventListener(button);
         this.clicked = false;
+        this.daysOfWeek = {
+            1:'Monday',
+            2:'Tuesday',
+            3:'Wednesday',
+            4:'Thursday',
+            5:'Friday',
+            6:'Saturday',
+            7:'Sunday',
+        }
     }
 
-    inputEventListener(element){
+    buttonEventListener(element){
         
         element.addEventListener('click' ,(e)=> {
             e.preventDefault();
@@ -16,14 +25,30 @@ export class ForecastButton extends Common {
             if(this.clicked){
                 this.changeVisibility(this.domElements['dailyForecastModule']);
                 this.clicked = !this.clicked;
-               
+            
             }
             else {
             this.clicked = !this.clicked;
-            fetch('http://api.weatherapi.com/v1/forecast.json?key=66667245d6d048b2ad9152824202510&q=07112&days=7')
+            fetch(`http://api.weatherapi.com/v1/forecast.json?key=66667245d6d048b2ad9152824202510&q=${this.domElements['cityName'].textContent}&days=4`)
             .then(data => data.json())
             .then(data => {
-                console.log(data)
+                console.log(data['forecast']['forecastday'])
+
+                Array.from(document.querySelectorAll('.weartherInfo__modal__dailyForecast__day')).map((element, index)=>{
+                    const arrayFromLink = data['forecast']['forecastday'][index]['day']['condition']['icon'].split('');
+                    const iconNumber = arrayFromLink.slice(arrayFromLink.length-7,arrayFromLink.length-4).join('');
+                  console.log(Math.floor((data['forecast']['forecastday'][index]['date_epoch']/86400)+4)%7)
+                  console.log( element.children[0]['children'][1]);
+
+                  
+                  element.children[0]['children'][1].textContent = this.daysOfWeek[Math.floor((data['forecast']['forecastday'][index]['date_epoch']/86400)+4)%7];
+                  element.children[0]['children'][0].src = `icons/day/${iconNumber}.png`;
+                  element.children[1].textContent = `${data['forecast']['forecastday'][index]['day']['avgtemp_c']}°C`;
+                })
+
+
+
+               
                 // (floor(T / 86400) + 4) mod 7
                  //(Math.floor((data['forecast']['forecastday'][1]['date_epoch']/86400)+4)%7)
                 this.changeVisibility(this.domElements['dailyForecastModule']);
