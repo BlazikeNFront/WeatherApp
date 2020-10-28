b803270d919c17474ee60e4bee9472259eb31d81
unknown <damian9691@wp.pl>
2020-10-28 20:15:13 +0100

    Added Charts logic and basic styles

--- src/inputQueryForecast.js
+++ src/inputQueryForecast.js
@@ -24,6 +24,7 @@ export class ForecastButton extends Common {
             
             if(this.clicked){
                 this.changeVisibility(this.domElements['dailyForecastModule']);
+                Array.from(document.querySelectorAll('.weartherInfo__modal__dailyForecast__day__temperatureChart')).forEach(element => element.innerHTML = '')
                 this.clicked = !this.clicked;
             
             }
@@ -32,25 +33,47 @@ export class ForecastButton extends Common {
             fetch(`http://api.weatherapi.com/v1/forecast.json?key=66667245d6d048b2ad9152824202510&q=${this.domElements['cityName'].textContent}&days=4`)
             .then(data => data.json())
             .then(data => {
-                console.log(data['forecast']['forecastday'])
-
-                Array.from(document.querySelectorAll('.weartherInfo__modal__dailyForecast__day')).map((element, index)=>{
+                
+                console.log(data)
+                Array.from(document.querySelectorAll('.weartherInfo__modal__dailyForecast__day')).map((element, index) => {
                     const arrayFromLink = data['forecast']['forecastday'][index]['day']['condition']['icon'].split('');
                     const iconNumber = arrayFromLink.slice(arrayFromLink.length-7,arrayFromLink.length-4).join('');
-                  console.log(Math.floor((data['forecast']['forecastday'][index]['date_epoch']/86400)+4)%7)
-                  console.log( element.children[0]['children'][1]);
-
-                  
+    
                   element.children[0]['children'][1].textContent = this.daysOfWeek[Math.floor((data['forecast']['forecastday'][index]['date_epoch']/86400)+4)%7];
                   element.children[0]['children'][0].src = `icons/day/${iconNumber}.png`;
-                  element.children[1].textContent = `${data['forecast']['forecastday'][index]['day']['avgtemp_c']}°C`;
+                  element.children[2].textContent = `${data['forecast']['forecastday'][index]['day']['avgtemp_c']}°C`;
+                  
                 })
 
+                Array.from(document.querySelectorAll('.weartherInfo__modal__dailyForecast__day__temperatureChart')).map((element,index) => {
+                 
+                    
+                    data['forecast']['forecastday'][index]['hour'].map((hour,hourindex) => {
+                      
+                        const box = document.createElement('div');
+                        const chart = document.createElement('div');
+                        const text = document.createElement('p');
+
+                        chart.classList.add('weartherInfo__modal__dailyForecast__day__temperatureChart__chart');
+                        chart.style.height = `${data['forecast']['forecastday'][index]['hour'][hourindex]['temp_c']/3}rem`;
+                        text.textContent = `${(data['forecast']['forecastday'][index]['hour'][hourindex]['temp_c']/3).toFixed(1)}°C`;
+
+                        box.appendChild(chart);
+                        box.appendChild(text);
+                        element.appendChild(box);
+
+                    })
+
+                   /*Array.from(element.children).map((element,hour,array) => {
+                    console.log(array.length)
+                         element.style.height = `${data['forecast']['forecastday'][index]['hour'][hour]['temp_c']/3}rem`
+                    })*/
+                   
+                })
+                
 
 
-               
-                // (floor(T / 86400) + 4) mod 7
-                 //(Math.floor((data['forecast']['forecastday'][1]['date_epoch']/86400)+4)%7)
+        
                 this.changeVisibility(this.domElements['dailyForecastModule']);
             })
             }
