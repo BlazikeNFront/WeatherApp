7e07878c85b39b8604a6afbd19ae9d527a87ae6d
unknown <damian9691@wp.pl>
2020-11-04 20:16:15 +0100

    Added logic for temperature below 0

--- src/inputQuery.js
+++ src/inputQuery.js
@@ -14,45 +14,7 @@ export class InputQuery extends Common {
                 element.addEventListener('click' ,(e)=> {
                  
                     e.preventDefault();
-                    new APICall(e.path[1][0], 'current')
-
-
-
-
-
-
-                    /*fetch(`http://api.weatherapi.com/v1/current.json?key=66667245d6d048b2ad9152824202510&q=${e.path[1][0].value}`)
-                    .then(data=> { return data.json()})
-                    .then(data=> { 
-                        e.path[1][0].value = '';
-                        const arrayFromLink = data['current']['condition']['icon'].split('');
-                        const iconNumber = arrayFromLink.slice(arrayFromLink.length-7,arrayFromLink.length-4).join('');
-                       
-                        if( data['current']['condition']['icon'].includes('day')){
-                        this.domElements['mainInfoIcon'].src=`icons/day/${iconNumber}.png`;
-                        }
-                            else { this.domElements['mainInfoIcon'].src = `icons/night/${113}.png` ;
-                        
-                        }  
-                        
-                        this.domElements['cityName'].textContent = data['location']['name'];
-                        this.domElements['weatherIconText'].textContent = data['current']['condition']['text'];
-                        this.domElements['additionalInfoHumidity'].textContent = `${data['current']['humidity']}%`;
-                        this.domElements['additionalInfoFeellike'].textContent = `${data['current']['feelslike_c']}°C`;
-                        this.domElements['additionalInfoWind'].textContent = `${data['current']['wind_kph']}km/h`;
-                        this.domElements['mainInfoTemperature'].textContent = `${data['current']['temp_c']}°C`;
-                        this.domElements['inputErrorMsg'].textContent = '';
-                        
-                      this.switchView(this.domElements['inputView'],this.domElements['modalView']);
-                    }).catch(error => {
-                        e.path[1][0].value = '';
-                        this.domElements['inputErrorMsg'].textContent = 'Wrong city!' ;
-
-                        
-                    });*/
-                    
-                    
-                    
+                    new APICall(e.path[1][0], 'current');
 
 
                     })}
